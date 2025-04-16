10937679f1f58a210ca05fd1ad93f70139e95982
shigekazukoya <shigekazukoya@gmail.com>
2025-04-17 00:24:32 +0900

    EditFileのDescription更新

--- FileSystem/FileSystemTools.cs
+++ FileSystem/FileSystemTools.cs
@@ -9,8 +9,8 @@ namespace FileSystem.Tools;
 [McpServerToolType]
 public static partial class FileSystemTools
 {
-    [McpServerTool,
-        Description("Edits a file by deleting a specified number of lines starting from a specific line position and then inserting new content at that position. For full file replacement, use lineNumber=1 and set linesToDelete to the total number of lines in the file.")]
+[McpServerTool,
+        Description("Edits a file by deleting a specified number of lines starting from a specific line position and then inserting new content at that position. For full file replacement, use lineNumber=1 and set linesToDelete to the total number of lines in the file. When executing EditFile multiple times, be careful with line numbers as they change after each edit. The line count may increase or decrease depending on content inserted and deleted.")]
     public static void EditFile(
         [Description("The path to the file to edit")] string filePath,
         [Description("The 1-based line number where the edit should start. Use 1 to start from the beginning of the file.")]
